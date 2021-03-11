93581005db22bbd78c4402055876374a6ecf51a9
alex chudsky <bowlder05@gmail.com>
2021-03-11 15:52:07 +0200

    current user endpoint

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,6 @@ const User = require('../models/user');
 exports.createOrUpdateUser = async (req, res) => {
     const {name, picture, email} = req.user;
 
-
     const user = await User.findOneAndUpdate(
         {email},
         {name: email.split('@')[0], picture},
@@ -25,5 +24,12 @@ exports.createOrUpdateUser = async (req, res) => {
         console.log('USER CREATED = ', newUser);
         res.json(newUser);
     }
+}
+
+exports.currentUser = async (req, res) => {
+    User.findOne({email: req.user.email}).exec((err, user) => {
+        if (err) throw new Error(err);
+        res.json(user);
+    })
 }
 
