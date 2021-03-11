886691031ca8b0f9106b0f42a483723d7d5a0cb1
alex chudsky <bowlder05@gmail.com>
2021-03-12 01:17:09 +0200

    admin route

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,4 +1,5 @@
 const admin = require('../firebase');
+const User = require('../models/user');
 
 exports.authCheck = async (req, res, next) => {
     try {
@@ -13,6 +14,19 @@ exports.authCheck = async (req, res, next) => {
         res.status(400).json({
             error: 'Invalid or expired token'
         })
+    }
+}
+
+exports.adminCheck = async (req, res, next) => {
+    const {user} = req.user;
+
+    const adminUser = await User.findOne({user});
+
+    if (adminUser.role !== 'admin') {
+        res.status(403).json({err: 'Admin resource. Access denied.',
+        });
+    } else {
+        next();
     }
 }
 
